0fe364a841948e95f45d38cb9f576c132c296a0b
fazza_abiyyu <fazza_abiyyu@qq.com>
2025-06-02 17:23:30 +0700

    Update db.ts

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,14 +1,2 @@
-// server/config/db.ts
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error'], // bisa tambahin 'query' buat debug dev
-  })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
